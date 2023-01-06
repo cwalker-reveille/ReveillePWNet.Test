e5abb8b94d70804f90117484de9d87115784bb81
Chris Walker <cwalker@reveillesoftware.com>
2023-01-06 14:26:24 -0500

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "ReveillePWNet.Test" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "ReveillePWNet.Test" )]
+[assembly: AssemblyCopyright( "Copyright ©  2023" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+
+[assembly: Guid( "47bc946d-1057-492a-8869-6958604514d9" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Playwright.MSTest;
+namespace ReveillePWNet.Test
+{
+    [TestClass]
+    public class UnitTest1 : PageTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int i = 1;
+            Assert.AreEqual(1, i);
+        }
+    }
+}
